4809d293496a10735c22d122306ff08895b572ab
Peter1907 <peter.nady.1408@gmail.com>
2023-02-15 00:43:47 +0200

    Add a home page with links to log-in & sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to home_index_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  resources :home, only: [:index]
   root "categories#index"
 end
