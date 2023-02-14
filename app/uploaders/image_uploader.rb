a0b281c89c71b5fd2796b97969b577325fd731bf
Peter1907 <peter.nady.1408@gmail.com>
2023-02-15 01:00:18 +0200

    Fix linter errors

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ require 'image_processing/mini_magick'
 class ImageUploader < Shrine
   Attacher.validate do
     validate_mime_type %w[image/jpeg image/png image/webp]
-    validate_max_size  1 * 1024 * 1024
+    validate_max_size 1 * 1024 * 1024
   end
 
   Attacher.derivatives do |original|
