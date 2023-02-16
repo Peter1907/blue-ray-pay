1cac29bff810172e799b8226a8cf88324d6f0c78
Peter1907 <peter.nady.1408@gmail.com>
2023-02-16 10:55:10 +0200

    Add tests for models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    user = User.new(name: "Peter", email: "peter@gmail.com", password: "123456", password_confirmation: "123456")
+    user.confirm
+    user.save
+    @category = Category.new(name: "Food", user_id: user.id)
+    @category.icon = File.open('./spec/assets/test.jpg')
+  end
+
+  it "should have a name" do
+    @category.name = nil
+    @category.save
+    expect(@category).to_not be_valid
+  end
+
+  it "should have an icon" do
+    @category.icon = nil
+    @category.save
+    expect(@category).to_not be_valid
+  end
+
+  it "should validate the icon" do
+    @category.icon = File.open('./spec/assets/test.txt')
+    @category.save
+    expect(@category).to_not be_valid
+  end
+
+  it "creates a thumbnail for the icon" do
+    @category.icon_derivatives!
+    @category.save
+    expect(@category.icon(:small).url).to be_present
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.new(name: "Peter", email: "peter@gmail.com", password: "123456", password_confirmation: "123456")
+  before { user.save }
+
+  it "should have a name" do
+    user.name = nil
+    user.confirm
+    expect(user).to_not be_valid
+  end
 end
